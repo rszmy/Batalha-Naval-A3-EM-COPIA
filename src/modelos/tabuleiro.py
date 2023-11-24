99cb3adf8ff434209a52e875d373e5f236343a8f
joao <joaorqn@gmail.com>
2023-11-23 22:27:15 -0300

    implementação da classe tabuleiro

--- src/modelos/tabuleiro.py
+++ src/modelos/tabuleiro.py
@@ -1,3 +1,5 @@
+# from modelos.embarcacao import Embarcacao, FabricaEmbarcacao
+
 class TabuleiroParte():
 
     _dict_alphanum = {
@@ -27,13 +29,13 @@ class TabuleiroParte():
     ]
 
     def set_quadrante(self, x, y, val):
-        cord_x = self._dict_alphanum[x]
-        cord_y = y - 1
-        self._matrix[cord_x][cord_y] = val
+        coord_x = self._dict_alphanum[x]
+        coord_y = y - 1
+        self._matrix[coord_x][coord_y] = val
 
     def get_quadrante(self, x, y):
-        cord_x = self._dict_alphanum[x]
-        return self._matrix[cord_x][y-1]
+        coord_x = self._dict_alphanum[x]
+        return self._matrix[coord_x][y-1]
     
 class Tabuleiro():
     
@@ -61,18 +63,30 @@ class Tabuleiro():
         try:
             if not self.checar_espaco(linha, coluna):
                 return True  # Embarcação não cabe no tabuleiro
-
+    # Embarcação não cabe no tabuleiro
             for i in range(10):
-                cord_x = chr(ord('A') + linha)
-                cord_y = coluna + i
-
-                if cord_x not in self._parte_a._dict_alphanum or cord_y >= len(self._parte_a._matrix[0]):
-                    return True  # Coordenadas fora dos limites
-
-                if self._parte_a.get_quadrante(cord_x, cord_y) != 'X':
-                    return True  # Navio sobrepondo navio
-
+                coord_x = chr(ord('A') + linha)
+                coord_y = coluna + i
+    # Coordenadas fora dos limites
+                if coord_x not in self._parte_a._dict_alphanum or coord_y >= len(self._parte_a._matrix[0]):
+                    return True 
+    # Navio sobrepondo navio
+                if self._parte_a.get_quadrante(coord_x, coord_y) != 'X':
+                    return True 
+    # Índicie fora dos limites do tabuleiro
         except IndexError:
-            return True  # Índicie fora dos limites do tabuleiro
-
+            return True
         return False
+
+    def numero_embarcacao():
+
+        embarcacoes = [
+            FabricaEmbarcacao.instance().create('Submarino')
+            FabricaEmbarcacao.instance().create('Submarino')
+            FabricaEmbarcacao.instance().create('Submarino')
+            FabricaEmbarcacao.instance().create('Navio Pequeno')
+            FabricaEmbarcacao.instance().create('Navio Pequeno')
+            FabricaEmbarcacao.instance().create('Navio Medio')
+            FabricaEmbarcacao.instance().create('Navio Grande')
+            FabricaEmbarcacao.instance().create('Porta Aviões')
+    ]:
