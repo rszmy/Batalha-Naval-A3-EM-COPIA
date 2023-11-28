33f23740327c5c0c6450076634d53ac6f6ad6d2c
Luigi <luigivibias11@gmail.com>
2023-11-27 21:33:27 -0300

    criando modelo da partida

---
+++ src/modelos/partida.py
@@ -0,0 +1,8 @@
+from modelos.tabuleiro import Tabuleiro
+from modelos.jogador import Jogador
+
+class Partida():
+    _jogador_a : Jogador
+    _jogador_b : Jogador
+    
+    _tabuleiro : Tabuleiro
