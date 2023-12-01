866caaccdf1433af6bb80d87cceb24977bc55229
nicolas <nicolas-sa-oliveira@hotmail.com>
2023-12-01 16:57:39 -0300

    correção topicos controlador

--- src/controladores/topicos_crontrolador.py
+++ src/controladores/topicos_crontrolador.py
@@ -1,18 +1,20 @@
 from Enum import Enum 
 
-from src.persistencia.TopicosDB import TopicoDB
+from src.persistencia.TopicoDB import TopicoDB
 from src.persistencia.jogador_db import JogadorDB
 
-from src.persistencia.TopicosDB import TopicoDB
+from src.persistencia.TopicoDB import TopicoDB
 from src.persistencia.jogador_db import JogadorDB
 
 class TopicoControladorErro(Enum):
     JOGADOR_NAO_EXISTENTE = 0 
     #======================================== retorna erro caso o jogador não esteja registrado e tente se inscrever
 
+
 class TopicoControlador:
+    
     def listar_topicos():
-        return Topico
+        pass
 
     def criar_novo_topico(nome):
         pass
@@ -21,16 +23,16 @@ class TopicoControlador:
         pass
 
     def subscribe(nome_topico, nome_jogador):
-    jogador : jogador = JogadorDB.get_by_name(nome_jogador)
+        jogador : jogador = JogadorDB.get_by_name(nome_jogador)
 
-    if(jogador != None):
-        return TopicoDB.subscribe(nome_topico, jogador)
-    else:
-        return TopicoControladorErro.JOGADOR_NAO_EXISTENTE
-
+        if(jogador != None):
+            return TopicoDB.subscribe(nome_topico, jogador)
+        else:
+            return TopicoControladorErro.JOGADOR_NAO_EXISTENTE
+    
     def unsubscribe(nome_topico, nome_jogador):    
         jogador : jogador = JogadorDB.get_by_name(nome_jogador)
-    if(jogador != None):
-        return TopicoDB.unsubscribe(nome_topico, jogador)
-    else:
-        return TopicoControladorErro.JOGADOR_NAO_EXISTENTE
+        if(jogador != None):
+            return TopicoDB.unsubscribe(nome_topico, jogador)
+        else:
+            return TopicoControladorErro.JOGADOR_NAO_EXISTENTE
