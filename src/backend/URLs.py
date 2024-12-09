02dd81bbb46b52adc662b1a01fcaa64c84e33b72
nicolas <nicolas-sa-oliveira@hotmail.com>
2024-12-09 01:33:56 -0300

    mudança origin

--- src/backend/URLs.py
+++ src/backend/URLs.py
@@ -9,6 +9,22 @@ import uvicorn
 
 app = FastAPI()
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # ========================== 
 
 if __name__ == "__main__":
