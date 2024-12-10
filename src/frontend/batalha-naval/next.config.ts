c9b9d7164a9429b87b85ef44823a978f779228b2
Bryan Szymczak <126414747+rszmy@users.noreply.github.com>
2024-12-10 01:31:50 -0300

    Update next.config.ts

--- src/frontend/batalha-naval/next.config.ts
+++ src/frontend/batalha-naval/next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  output: 'standalone',
+  // If you're having CORS issues, you might need this
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' }
+        ],
+      },
+    ]
+  }
 };
 
 export default nextConfig;
